f09cc5ae6e59f0cae23639a5a5ab0dfb0b1090a4
l08084 <l08084.1989@gmail.com>
2020-05-25 18:53:51 +0900

    update

--- src/App.java
+++ src/App.java
@@ -1,54 +1,36 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        // (1) 正規表現のパターンを生成
-        Pattern pattern = Pattern.compile("[0-9]+");
+        // (1) 正規表現の文字列を定義
+        String regex = "[0-9]+";
 
-        // (2) 正規表現のパターンに適合するかをチェックする文字列
+        // (2) 正規表現のパターンに適合するかをチェックする文字列を定義
         String sentence1 = "12";
 
-        // (3) 正規表現処理をおこなうためのクラスを取得
-        Matcher matcher1 = pattern.matcher(sentence1);
+        // (3) 正規表現のパターンに適合するかチェック
+        System.out.println(sentence1.matches(regex));
 
-        // (4) 正規表現のパターンに適合するかチェック
-        System.out.println(matcher1.matches());
-
-        // (2) 正規表現のパターンに適合するかをチェックする文字列
+        // (2) 正規表現のパターンに適合するかをチェックする文字列を定義
         String sentence2 = "1a2";
 
-        // (3) 正規表現処理をおこなうためのクラスを取得
-        Matcher matcher2 = pattern.matcher(sentence2);
+        // (3) 正規表現のパターンに適合するかチェック
+        System.out.println(sentence2.matches(regex));
 
-        // (4) 正規表現のパターンに適合するかチェック
-        System.out.println(matcher2.matches());
-
-        // (2) 正規表現のパターンに適合するかをチェックする文字列
+        // (2) 正規表現のパターンに適合するかをチェックする文字列を定義
         String sentence3 = "1.0";
 
-        // (3) 正規表現処理をおこなうためのクラスを取得
-        Matcher matcher3 = pattern.matcher(sentence3);
+        // (3) 正規表現のパターンに適合するかチェック
+        System.out.println(sentence3.matches(regex));
 
-        // (4) 正規表現のパターンに適合するかチェック
-        System.out.println(matcher3.matches());
-
-        // (2) 正規表現のパターンに適合するかをチェックする文字列
+        // (2) 正規表現のパターンに適合するかをチェックする文字列を定義
         String sentence4 = "-1";
 
-        // (3) 正規表現処理をおこなうためのクラスを取得
-        Matcher matcher4 = pattern.matcher(sentence4);
+        // (3) 正規表現のパターンに適合するかチェック
+        System.out.println(sentence4.matches(regex));
 
-        // (4) 正規表現のパターンに適合するかチェック
-        System.out.println(matcher4.matches());
-
-        // (2) 正規表現のパターンに適合するかをチェックする文字列
+        // (2) 正規表現のパターンに適合するかをチェックする文字列を定義
         String sentence5 = "485617";
 
-        // (3) 正規表現処理をおこなうためのクラスを取得
-        Matcher matcher5 = pattern.matcher(sentence5);
-
-        // (4) 正規表現のパターンに適合するかチェック
-        System.out.println(matcher5.matches());
+        // (3) 正規表現のパターンに適合するかチェック
+        System.out.println(sentence5.matches(regex));
     }
 }
